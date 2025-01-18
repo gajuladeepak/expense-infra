c3985d6ee1390ef76fc67ba0a15b41a472cc84d1
deepakgajula <gajuladeepak17@gmail.com>
2025-01-18 18:31:04 +0530

    60-alb

--- 60-alb/data.tf
+++ 60-alb/data.tf
@@ -9,10 +9,10 @@ data "aws_ssm_parameter" "public_subnet_ids" {  #this will return us string list
 }
 
 
-data "aws_ssm_parameter" "web_alb_sg_id" {  #this will return us string list #we can't do our operation on string_list we need to convert string list to list
-  # /expense/dev/app_alb_sg_id  #i want name in this format
-  name  = "/${var.project_name}/${var.environment}/web_alb_sg_id"
+data "aws_ssm_parameter" "ingress_alb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
 }
+
 
 
 data "aws_ssm_parameter" "https_certificate_arn" {  #this will return us string list #we can't do our operation on string_list we need to convert string list to list
