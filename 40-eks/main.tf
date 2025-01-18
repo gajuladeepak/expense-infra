4ac533145dd4f8267a3fb1fce4bdeaf65afd3b96
deepakgajula <gajuladeepak17@gmail.com>
2025-01-18 18:02:50 +0530

    40-eks

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -57,13 +57,13 @@ module "eks" {
       min_size      = 2
       max_size      = 10
       desired_size  = 2
-      #capacity_type = "SPOT"
+      capacity_type = "SPOT"
       iam_role_additional_policies = {
         AmazonEBSCSIDriverPolicy          = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonElasticFileSystemFullAccess = "arn:aws:iam::aws:policy/AmazonEFSCSIDriverPolicy"
+        AmazonElasticFileSystemFullAccess = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
         ElasticLoadBalancingFullAccess = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+
       }
-      # EKS takes AWS Linux 2 as it's OS to the nodes
       key_name = aws_key_pair.eks.key_name
     }
   }
